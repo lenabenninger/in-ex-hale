a2aa0b0500ab26d8103463ddf71c8fdb1ebd4f16
lenabenninger <lena.benninger@studentfr.ch>
2024-09-19 17:13:15 +0200

    gsap title in.

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,21 @@ import gsap from "gsap";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+
+const titleIn = document.querySelector("#title-in");
+const titleCircleOne = document.querySelector("#title-circle-one");
+const titleEx = document.querySelector("#title-ex");
+const titleCircleTwo = document.querySelector("#title-circle-two");
+const titleHale = document.querySelector("#title-hale");
+
+const tlTitle = gsap.timeline();
+
+tlTitle.from(titleIn, {
+  opacity: 0,
+  duration: 3,
+});
+
+tlTitle.from(titleCircleOne, {
+  opacity: 0,
+  duration: 3,
+});
