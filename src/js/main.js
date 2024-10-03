7c7477e53cf71f8f295f5661fccbb401e92b456a
lenabenninger <lena.benninger@studentfr.ch>
2024-10-03 17:03:10 +0200

    select duration + button start

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,64 @@ const titleCircleOne = document.querySelector("#title-circle-one");
 const titleEx = document.querySelector("#title-ex");
 const titleCircleTwo = document.querySelector("#title-circle-two");
 const titleHale = document.querySelector("#title-hale");
+const titleSubtitle = document.querySelector(".title-subtitle");
+const boxTitle = document.querySelector(".box-title");
+const boxTimer = document.querySelector(".timer");
+const timeSelected = document.querySelectorAll(".time");
+const startBtn = document.querySelector(".btn");
 
 const tlTitle = gsap.timeline();
 
 tlTitle.from(titleIn, {
   opacity: 0,
-  duration: 3,
+  duration: 0.5,
 });
 
 tlTitle.from(titleCircleOne, {
   opacity: 0,
-  duration: 3,
+  duration: 0.5,
+});
+
+tlTitle.from(titleEx, {
+  opacity: 0,
+  duration: 0.5,
+});
+
+tlTitle.from(titleCircleTwo, {
+  opacity: 0,
+  duration: 0.5,
+});
+
+tlTitle.from(titleHale, {
+  opacity: 0,
+  duration: 0.5,
+});
+
+tlTitle.from(titleSubtitle, {
+  opacity: 0,
+  duration: 0.5,
+});
+
+tlTitle.to(boxTitle, {
+  opacity: 0,
+  duration: 0.6,
+});
+
+tlTitle.from(
+  boxTimer,
+  {
+    opacity: 0,
+    duration: 1.5,
+  },
+  "<"
+);
+
+timeSelected.forEach(function (element) {
+  element.addEventListener("click", function () {
+    element.classList.add("active");
+  });
+});
+
+startBtn.addEventListener("click", function () {
+  boxTimer.style.opacity = "0";
 });
